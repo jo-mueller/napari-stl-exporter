10295d701e2a15c45d12559856fddbae4fd64205
Johannes Müller <38459088+jo-mueller@users.noreply.github.com>
2022-04-06 16:46:17 +0200

    Added new files to init

--- napari_stl_exporter/__init__.py
+++ napari_stl_exporter/__init__.py
@@ -1,4 +1,10 @@
 
 __version__ = "0.0.6"
 
-from ._writer import napari_get_writer, napari_write_labels
+from ._writer import napari_get_writer, napari_write_labels, napari_write_surfaces
+from napari_plugin_engine import napari_hook_implementation
+from ._image_to_surface import image_to_surface
+
+@napari_hook_implementation
+def napari_experimental_provide_dock_widget():
+    return image_to_surface
