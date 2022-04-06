7eef46acba2435dd5f3d203d5381d7fcf9cb6e31
Johannes Müller <38459088+jo-mueller@users.noreply.github.com>
2022-04-06 18:04:20 +0200

    added test for surface export

--- napari_stl_exporter/_tests/test_writer.py
+++ napari_stl_exporter/_tests/test_writer.py
@@ -1,4 +1,4 @@
-from napari_stl_exporter._writer import _labels_to_mesh, napari_write_labels
+from napari_stl_exporter._writer import _labels_to_mesh, napari_write_labels, napari_write_surface
 import numpy as np
 import os
 
@@ -12,6 +12,7 @@ def test_label_conversion():
     assert isinstance(mesh, vedo.mesh.Mesh)
 
 def test_writer(tmpdir):
+    from skimage import measure
     label_image = np.zeros((100, 100, 100))
     label_image[25:75, 25:75, 25:75] = 1
 
@@ -24,3 +25,15 @@ def test_writer(tmpdir):
     ply_file = napari_write_labels(pth, label_image, None)
     assert os.path.exists(pth)
     assert ply_file is not None
+
+    surf = measure.marching_cubes(label_image)
+
+    pth = os.path.join(str(tmpdir), "test_export.ply")
+    stl_file = napari_write_surface(pth, surf, None)
+    assert os.path.exists(pth)
+    assert stl_file is not None
+
+    pth = os.path.join(str(tmpdir), "test_export.stl")
+    stl_file = napari_write_surface(pth, surf, None)
+    assert os.path.exists(pth)
+    assert stl_file is not None
