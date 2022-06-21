843c8168458b87ad3de8f9fb79fea4d5ea14a46b
Johannes Müller <38459088+jo-mueller@users.noreply.github.com>
2022-06-21 14:32:24 +0200

    Added tests

--- src/napari_stl_exporter/_tests/test_writer_and_readers.py
+++ src/napari_stl_exporter/_tests/test_writer_and_readers.py
@@ -3,6 +3,8 @@ from napari_stl_exporter._test_data import make_pyramid_label, make_pyramid_surf
 import numpy as np
 import os
 
+supported_formats = ['.stl', '.ply', '.obj']
+
 def test_label_conversion():
     import vedo
     label_image = np.zeros((100, 100, 100))
@@ -17,27 +19,19 @@ def test_writer(tmpdir):
     label_image = np.zeros((100, 100, 100))
     label_image[25:75, 25:75, 25:75] = 1
 
-    pth = os.path.join(str(tmpdir), "test_export.stl")
-    stl_file = napari_write_labels(pth, label_image, None)
-    assert os.path.exists(pth)
-    assert stl_file is not None
-
-    pth = os.path.join(str(tmpdir), "test_export.ply")
-    ply_file = napari_write_labels(pth, label_image, None)
-    assert os.path.exists(pth)
-    assert ply_file is not None
+    for ext in supported_formats:
+        pth = os.path.join(str(tmpdir), "test_export" + ext)
+        stl_file = napari_write_labels(pth, label_image, None)
+        assert os.path.exists(pth)
+        assert stl_file is not None
 
     surf = measure.marching_cubes(label_image)
 
-    pth = os.path.join(str(tmpdir), "test_export.ply")
-    stl_file = napari_write_surface(pth, surf, None)
-    assert os.path.exists(pth)
-    assert stl_file is not None
-
-    pth = os.path.join(str(tmpdir), "test_export.stl")
-    stl_file = napari_write_surface(pth, surf, None)
-    assert os.path.exists(pth)
-    assert stl_file is not None
+    for ext in supported_formats:
+        pth = os.path.join(str(tmpdir), "test_export" + ext)
+        stl_file = napari_write_surface(pth, surf, None)
+        assert os.path.exists(pth)
+        assert stl_file is not None
 
 def test_writer_viewer(make_napari_viewer, tmpdir):
     import napari
@@ -64,12 +58,12 @@ def test_reader(make_napari_viewer, tmpdir):
     import napari_stl_exporter
 
     pyramid = napari_stl_exporter.make_pyramid_surface()[0][0]
-
-    napari_stl_exporter.napari_write_surface(os.path.join(tmpdir, 'test.stl'), pyramid, None)
-    pyramid = napari_stl_exporter.napari_import_surface(os.path.join(tmpdir, 'test.stl'))
-
     viewer = make_napari_viewer()
-    viewer.add_surface(pyramid)
+
+    for ext in supported_formats:
+        napari_stl_exporter.napari_write_surface(os.path.join(tmpdir, 'test' + ext), pyramid, None)
+        pyramid = napari_stl_exporter.napari_import_surface(os.path.join(tmpdir, 'test' + ext))
+        viewer.add_surface(pyramid)
 
 
 
