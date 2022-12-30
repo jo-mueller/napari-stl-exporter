eb026866e122d7285a8da6b1581c6e5d0fe9f915
Johannes Müller <johannes.mueller88@hotmail.de>
2022-12-30 14:39:19 +0100

    added extrude function

--- src/napari_stl_exporter/_image_to_surface.py
+++ src/napari_stl_exporter/_image_to_surface.py
@@ -3,7 +3,31 @@ from itertools import product
 from magicgui import magic_factory
 
 @magic_factory
-def image_to_surface(image:ImageData, z_multiplier: float = 1.0) -> SurfaceData:
+def extrude(surface: SurfaceData, distance: float = 1.0) -> SurfaceData:
+    """
+    Extrude a mesh by a given value.
+
+    This offsets all vertices in a mesh by a given value. New faces will be created
+    at the edge of non-closed meshs, thus solidifying meshes.
+    
+    Parameters
+    ----------
+    surface: SUrfaceData
+
+    Returns
+    -------
+    surface: SurfaceData
+    """
+    import vedo
+    import numpy as np
+
+    mesh = vedo.mesh.Mesh((np.flip(surface[0], axis=1), surface[1]))
+    mesh = mesh.extrude(zshift=distance)
+
+    return (np.flip(mesh.points(), axis=1), np.asarray(mesh.faces()))
+
+@magic_factory
+def image_to_surface(image: ImageData, z_multiplier: float = 1.0) -> SurfaceData:
     """
     Convert a 2D image to a surface mesh. The image intensity is used as z-coordinate of the mesh.
 
@@ -12,6 +36,11 @@ def image_to_surface(image:ImageData, z_multiplier: float = 1.0) -> SurfaceData:
     image : ImageData
 
     z_multiplier: Multiplies the intensity-derived z-coordinate with this factor.
+    
+    Returns
+    -------
+
+    surface: SUrfaceData
     """
 
     import numpy as np
